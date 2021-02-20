2b75e5a6fb702aa01973cbec440e6c67da632e05
Kazuhiro Takahashi <pupodenews@gmail.com>
2021-02-20 13:04:57 +0900

    BoostrapperShell

---
+++ BootstrapperShell/BootstrapperShell/Boostrapper.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BootstrapperShell
+{
+    class Boostrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
